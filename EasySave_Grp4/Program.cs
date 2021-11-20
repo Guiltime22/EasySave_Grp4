0573ae8cfe863e444ca5b2dc85a3b63dca097478
Guiltime22 <ghiles.ouchaoua.dz@viacesi.fr>
2021-11-20 20:23:52 +0100

    Ajoutez des fichiers projet.

---
+++ EasySave_Grp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez saisir votre dossier source");
+            string src = Console.ReadLine();
+            Console.WriteLine("Veuillez saisir votre dossier destination");
+            string dest = Console.ReadLine();
+
+            Sauvegarde SV = new Sauvegarde();
+            SV.CopyRepertoire(src, dest);
+        }
+    }
+}
---
+++ EasySave_Grp4/Sauvegarde.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace test
+{
+    public class Sauvegarde
+    {
+        public void CopyRepertoire(string src, string dest)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(src);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + src);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(dest);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(dest, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+            /*
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(dest, subdir.Name);
+                    CopyRepertoire(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+            */
+        }
+    }
+}
