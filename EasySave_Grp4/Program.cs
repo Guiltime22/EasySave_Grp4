558ae22422adf418ef936e8ef272636625ccbec3
Guiltime22 <ghiles.ouchaoua.dz@viacesi.fr>
2021-11-21 10:37:39 +0100

    Ghiles

--- EasySave_Grp4/Program.cs
+++ EasySave_Grp4/Program.cs
@@ -4,15 +4,51 @@ namespace test
 {
     class Program
     {
+        static string src;
+        static string dest;
+        static int i;
         static void Main(string[] args)
         {
-            Console.WriteLine("Veuillez saisir votre dossier source");
-            string src = Console.ReadLine();
-            Console.WriteLine("Veuillez saisir votre dossier destination");
-            string dest = Console.ReadLine();
+            
+            Console.WriteLine($"Bienvenue dans l'interface EasySave {Environment.NewLine}");
 
-            Sauvegarde SV = new Sauvegarde();
-            SV.CopyRepertoire(src, dest);
+            Console.WriteLine($"1. Création Travail de sauvegarde {Environment.NewLine} 2. Execution Travail de sauvegarde {Environment.NewLine} 3. Exit");
+            
+            Console.WriteLine("Votre saisie:");
+            int Input = Convert.ToInt32(Console.ReadLine());
+
+            if(Input == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Combien de travaux voulez-vous réaliser ?");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                Sauvegarde SV = new Sauvegarde();
+                SV.Travail_Sauvegarde(choice);
+                /*
+                Console.WriteLine("Veuillez saisir votre dossier source");
+                src = Console.ReadLine();
+                Console.WriteLine("Veuillez saisir votre dossier destination");
+                dest = Console.ReadLine();
+
+                Sauvegarde SV = new Sauvegarde();
+                SV.CopyRepertoire(src, dest);
+                */
+            }
+            else if(Input == 2)
+            {
+                Console.Clear();
+            }
+            else if(Input == 3)
+            {
+                Console.Clear();
+                Environment.Exit(0);
+            }else
+            {
+                Console.WriteLine("Choix incorrecte, fermeture de la console");
+                Environment.Exit(0);
+            }
+            
         }
     }
 }
--- EasySave_Grp4/Sauvegarde.cs
+++ EasySave_Grp4/Sauvegarde.cs
@@ -1,10 +1,58 @@
 ﻿using System;
 using System.IO;
+//using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace test
 {
+    public class JFile
+    {
+        public string name { get; set; }
+        public string source_name { get; set; }
+        public string dest_name { get; set; }
+        public string type_save { get; set; }
+    }
     public class Sauvegarde
     {
+        public void Travail_Sauvegarde(int choice)
+        {
+            while (choice > 5)
+            {
+                Console.WriteLine("Vous avez depassé le seuil de travaux, veuillez réessayer ultérieurement");
+            }
+            for (int i = 1; i <= choice; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("Veuillez saisir le nom du travail");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Veuillez saisir le dossier source");
+                string source_name = Console.ReadLine();
+
+                Console.WriteLine("Veuillez saisir le dossier destination");
+                string dest_name = Console.ReadLine();
+
+                Console.WriteLine("Veuillez saisir le type de sauvegarde");
+                string type_save = Console.ReadLine();
+
+                var jFile = new JFile
+                {
+                    name = name,
+                    source_name = source_name,
+                    dest_name = dest_name,
+                    type_save = type_save
+                };
+
+                string fileName = @"C:\Users\ghile\Desktop\JFile"+i+".json";
+                string jsonString = JsonSerializer.Serialize(jFile);
+                File.WriteAllText(fileName, jsonString);
+
+                //Console.WriteLine(jsonString);
+            }
+            
+
+        }
+        
         public void CopyRepertoire(string src, string dest)
         {
             // Get the subdirectories for the specified directory.
