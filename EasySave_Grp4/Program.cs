3f8cdf38499ebe9db1bb01afe4aec55a8a4d8208
Chiraz <DELL@CHIRAZDDC>
2021-11-24 14:53:59 +0100

    Adding Log

--- EasySave_Grp4/Program.cs
+++ EasySave_Grp4/Program.cs
@@ -1,54 +1,79 @@
 ﻿using System;
+using System.IO;
+//using Newtonsoft.Json;
+using System.Text.Json;
 
-namespace sauvegarde
+namespace test
 {
     class Program
     {
-        static string src;
-        static string dest;
-        static int i;
+        static string path = @"..\..\..\Config\Travaux_Sauvegarde\";
+
         static void Main(string[] args)
         {
-            
-            Console.WriteLine($"Bienvenue dans l'interface EasySave {Environment.NewLine}");
-
-            Console.WriteLine($"1. Création Travail de sauvegarde {Environment.NewLine} 2. Execution Travail de sauvegarde {Environment.NewLine} 3. Exit");
-            
-            Console.WriteLine("Votre saisie:");
-            int Input = Convert.ToInt32(Console.ReadLine());
-
-            if(Input == 1)
+            Console.Clear();
+            while (true)
             {
-                Console.Clear();
-                Console.WriteLine("Combien de travaux voulez-vous réaliser ?");
-                int choice = Convert.ToInt32(Console.ReadLine());
+                int nbFichiersSD = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly).Length;
 
                 Sauvegarde SV = new Sauvegarde();
-                SV.Travail_Sauvegarde(choice);
-                /*
-                Console.WriteLine("Veuillez saisir votre dossier source");
-                src = Console.ReadLine();
-                Console.WriteLine("Veuillez saisir votre dossier destination");
-                dest = Console.ReadLine();
+                Console.WriteLine("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+                Console.WriteLine("▒▒                                                                                                         ▒▒");
+                Console.WriteLine("▒▒ ▒▒▒▒▒▒▒▒▒        ▒        ▒▒▒▒▒▒▒▒  ▒▒▒      ▒▒▒  ▒▒▒▒▒▒▒▒          ▒       ▒▒            ▒▒ ▒▒▒▒▒▒▒▒▒▒ ▒▒");
+                Console.WriteLine("▒▒ ▒▒              ▒▒▒      ▒▒▒         ▒▒▒    ▒▒▒  ▒▒▒               ▒▒▒       ▒▒          ▒▒  ▒▒         ▒▒");
+                Console.WriteLine("▒▒ ▒▒             ▒▒ ▒▒     ▒▒▒          ▒▒▒  ▒▒▒   ▒▒▒              ▒▒ ▒▒       ▒▒        ▒▒   ▒▒         ▒▒");
+                Console.WriteLine("▒▒ ▒▒▒▒▒▒▒▒▒     ▒▒   ▒▒     ▒▒▒▒▒▒▒▒     ▒▒▒▒▒▒     ▒▒▒▒▒▒▒▒       ▒▒   ▒▒       ▒▒      ▒▒    ▒▒▒▒▒▒▒▒▒▒ ▒▒");
+                Console.WriteLine("▒▒ ▒▒           ▒▒▒▒▒▒▒▒▒          ▒▒▒      ▒▒             ▒▒▒     ▒▒▒▒▒▒▒▒▒       ▒▒    ▒▒     ▒▒         ▒▒");
+                Console.WriteLine("▒▒ ▒▒          ▒▒▒     ▒▒▒         ▒▒▒      ▒▒             ▒▒▒    ▒▒▒     ▒▒▒       ▒▒  ▒▒      ▒▒         ▒▒");
+                Console.WriteLine("▒▒ ▒▒▒▒▒▒▒▒▒  ▒▒▒       ▒▒▒  ▒▒▒▒▒▒▒▒       ▒▒       ▒▒▒▒▒▒▒▒    ▒▒▒       ▒▒▒       ▒▒▒▒       ▒▒▒▒▒▒▒▒▒▒ ▒▒");
+                Console.WriteLine("▒▒                                                                                                         ▒▒");
+                Console.WriteLine("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+                Console.WriteLine($"Bienvenue dans l'interface EasySave => vous avez " + nbFichiersSD + $"/5 slots de sauvegardes. {Environment.NewLine}");
 
-                Sauvegarde SV = new Sauvegarde();
-                SV.CopyRepertoire(src, dest);
-                */
+                Console.WriteLine($"1. Création Travail de sauvegarde                  2. Execution Travail de sauvegarde {Environment.NewLine} {Environment.NewLine}" +
+                                  $"3. Gestion des travaux de sauvegarde               4. Exit {Environment.NewLine}");
+
+                Console.WriteLine("Votre saisie:");
+                int Input = Convert.ToInt32(Console.ReadLine());
+
+                if (Input == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Combien de travaux voulez-vous réaliser ?");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice + nbFichiersSD <= 5)
+                    {
+                        SV.Create_Travail_Sauvegarde(choice, nbFichiersSD);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nombre de slots insuffisant, veuillez réessayer ultérieurement");
+                        Environment.Exit(0);
+                    }
+
+                }
+                else if (Input == 2)
+                {
+                    Console.Clear();
+                    SV.Execute_Travail_Sauvegarde();
+                }
+                else if (Input == 3)
+                {
+                    Console.Clear();
+                    SV.Gestion_Travail_Sauvegarde(nbFichiersSD);
+                }
+                else if (Input == 4)
+                {
+                    Console.Clear();
+                    Console.WriteLine("BYE BYE");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Choix incorrecte, fermeture de la console");
+                    Environment.Exit(0);
+                }
             }
-            else if(Input == 2)
-            {
-                Console.Clear();
-            }
-            else if(Input == 3)
-            {
-                Console.Clear();
-                Environment.Exit(0);
-            }else
-            {
-                Console.WriteLine("Choix incorrecte, fermeture de la console");
-                Environment.Exit(0);
-            }
-            
         }
     }
 }
--- EasySave_Grp4/Sauvegarde.cs
+++ EasySave_Grp4/Sauvegarde.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.IO;
-//using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System.Text.Json;
+using System.Collections.Generic;
 
 namespace test
 {
@@ -14,80 +15,230 @@ namespace test
     }
     public class Sauvegarde
     {
-        public void Travail_Sauvegarde(int choice)
+
+        private Type_Save SV = new Type_Save();
+        public void Create_Travail_Sauvegarde(int choice, int nbFichiersSD)
         {
-            while (choice > 5)
+            if (nbFichiersSD <= 5)
             {
-                Console.WriteLine("Vous avez depassé le seuil de travaux, veuillez réessayer ultérieurement");
+                while (choice > 5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Vous avez depassé le seuil de travaux, veuillez réessayer ultérieurement");
+                    return;
+                }
+                for (int i = 1; i <= choice; i++)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Veuillez saisir le dossier source");
+                    string source_name = Console.ReadLine();
+                    try
+                    {
+                        var verifDest = Directory.GetFiles(source_name, "*", SearchOption.AllDirectories);
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Dossier introuvable, veuillez réessayer");
+                        return;
+                    }
+                    Console.WriteLine("Veuillez saisir le dossier destination");
+                    string dest_name = Console.ReadLine();
+
+                    Console.WriteLine("Veuillez saisir le type de sauvegarde : Complet / Differentiel");
+                    string type_save = Console.ReadLine();
+
+                    Console.WriteLine("Veuillez saisir le nom du travail");
+                    string name = Console.ReadLine();
+
+
+
+                    var jFile = new JFile
+                    {
+                        name = name,
+                        source_name = source_name,
+                        dest_name = dest_name,
+                        type_save = type_save
+                    };
+
+                    string fileName = @"..\..\..\Config\Travaux_Sauvegarde\" + name + ".json";
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(jFile);
+                    File.WriteAllText(fileName, jsonString);
+                    Console.Clear();
+                    //Console.WriteLine(jsonString);
+                }
+                Console.WriteLine("Travail de sauvegarde enrengistré avec succès !");
             }
-            for (int i = 1; i <= choice; i++)
+            else
             {
                 Console.Clear();
-                Console.WriteLine("Veuillez saisir le nom du travail");
-                string name = Console.ReadLine();
-
-                Console.WriteLine("Veuillez saisir le dossier source");
-                string source_name = Console.ReadLine();
-
-                Console.WriteLine("Veuillez saisir le dossier destination");
-                string dest_name = Console.ReadLine();
-
-                Console.WriteLine("Veuillez saisir le type de sauvegarde");
-                string type_save = Console.ReadLine();
-
-                var jFile = new JFile
-                {
-                    name = name,
-                    source_name = source_name,
-                    dest_name = dest_name,
-                    type_save = type_save
-                };
-
-                string fileName = @"C:\Users\ghile\Desktop\JFile"+i+".json";
-                string jsonString = JsonSerializer.Serialize(jFile);
-                File.WriteAllText(fileName, jsonString);
-
-                //Console.WriteLine(jsonString);
+                Console.WriteLine("Limite de nombres de travaux atteints, veuillez libérer de l'espace");
+                return;
             }
-            
-
+            return;
         }
-        
-        public void CopyRepertoire(string src, string dest)
+
+        public void Execute_Travail_Sauvegarde()
         {
-            // Get the subdirectories for the specified directory.
-            DirectoryInfo dir = new DirectoryInfo(src);
-
-            if (!dir.Exists)
+            Console.WriteLine("Choisissez le mode d'execution");
+            Console.WriteLine($"1. Unique                                2. Sequentielle {Environment.NewLine}" +
+                              $"3. Exit");
+            int EXE = Convert.ToInt32(Console.ReadLine());
+            if (EXE == 1)
             {
-                throw new DirectoryNotFoundException(
-                    "Source directory does not exist or could not be found: "
-                    + src);
-            }
-
-            DirectoryInfo[] dirs = dir.GetDirectories();
-
-            // If the destination directory doesn't exist, create it.       
-            Directory.CreateDirectory(dest);
-
-            // Get the files in the directory and copy them to the new location.
-            FileInfo[] files = dir.GetFiles();
-            foreach (FileInfo file in files)
-            {
-                string tempPath = Path.Combine(dest, file.Name);
-                file.CopyTo(tempPath, false);
-            }
-            /*
-            // If copying subdirectories, copy them and their contents to new location.
-            if (copySubDirs)
-            {
-                foreach (DirectoryInfo subdir in dirs)
+                Console.Clear();
+                string[] files = Directory.GetFiles(@"..\..\..\Config\Travaux_Sauvegarde", "*.json");
+                Console.WriteLine("Vos Travaux : ");
+                foreach (string file in files)
                 {
-                    string tempPath = Path.Combine(dest, subdir.Name);
-                    CopyRepertoire(subdir.FullName, tempPath, copySubDirs);
+                    JFile test = JsonConvert.DeserializeObject<JFile>(File.ReadAllText(file));
+                    Console.WriteLine("╔══════════════════════════════════════════════════════════════════════════════════════╗");
+                    Console.WriteLine("Nom :" + test.name);
+                    Console.WriteLine("Dossier Source :" + test.source_name);
+                    Console.WriteLine("Dossier Destinataire :" + test.dest_name);
+                    Console.WriteLine("Type de Sauvegarde :" + test.type_save);
+                    Console.WriteLine("╚══════════════════════════════════════════════════════════════════════════════════════╝");
+                }
+                Console.WriteLine("Veuillez saisir le nom du travail de sauvegarde");
+                try
+                {
+                    string nom_fichier = Console.ReadLine();
+                    string fileName = @"..\..\..\Config\Travaux_Sauvegarde\" + nom_fichier + ".json";
+                    string jsonString = File.ReadAllText(Path.GetFullPath(fileName));
+                    
+                    JFile jFile = System.Text.Json.JsonSerializer.Deserialize<JFile>(jsonString);
+                    if (jFile.type_save == "Complet")
+                    {
+
+                        SV.CopyRepertoire(nom_fichier, jFile.source_name, jFile.dest_name);
+                        File.Delete(fileName);
+                        Console.Clear();
+                        Console.WriteLine("Copie effectué avec succès !");
+
+                    }
+                    else if (jFile.type_save == "Differentiel")
+                    {
+
+                        SV.CopyRepertoire_Modifier(jFile.source_name, jFile.dest_name);
+                        File.Delete(fileName);
+                        Console.WriteLine("Copie effectué avec succès !");
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Travail introuvable, veuillez réessayer");
+                    return;
                 }
             }
-            */
+            if (EXE == 2)
+            {
+                Console.Clear();
+                string[] files = Directory.GetFiles(@"..\..\..\Config\Travaux_Sauvegarde", "*.json");
+                foreach (var file in files)
+                {
+                    string jsonString = File.ReadAllText(file);
+                    JFile jFile = System.Text.Json.JsonSerializer.Deserialize<JFile>(jsonString);
+                    if (jFile.type_save == "Complet")
+                    {
+                        string nom_fichier = jFile.name;
+                        SV.CopyRepertoire(nom_fichier, jFile.source_name, jFile.dest_name);
+                        File.Delete(file);
+                    }
+                    else if (jFile.type_save == "Differentiel")
+                    {
+                        SV.CopyRepertoire_Modifier(jFile.source_name, jFile.dest_name);
+                        File.Delete(file);
+                        Console.Clear();
+                    }
+                }
+
+                Console.WriteLine("Copie effectué avec succès !");
+            }
+            if (EXE == 3)
+            {
+                Console.Clear();
+                return;
+            }
+            return;
         }
+        public void Gestion_Travail_Sauvegarde(int nbFichiersSD)
+        {
+            Console.WriteLine("Faites votre choix !");
+            Console.WriteLine($"1. Afficher                                2. Modifier {Environment.NewLine} {Environment.NewLine}" +
+                               "3. Supprimer                               4. Exit");
+            int Gest = Convert.ToInt32(Console.ReadLine());
+            if (Gest == 1)
+            {
+                Console.Clear();
+                string[] files = Directory.GetFiles(@"..\..\..\Config\Travaux_Sauvegarde", "*.json");
+                Console.WriteLine("Vos Travaux : ");
+                foreach (string file in files)
+                {
+                    JFile test = JsonConvert.DeserializeObject<JFile>(File.ReadAllText(file));
+                    Console.WriteLine("╔══════════════════════════════════════════════════════════════════════════════════════╗");
+                    Console.WriteLine("Nom :" + test.name);
+                    Console.WriteLine("Dossier Source :" + test.source_name);
+                    Console.WriteLine("Dossier Destinataire :" + test.dest_name);
+                    Console.WriteLine("Type de Sauvegarde :" + test.type_save);
+                    Console.WriteLine("╚══════════════════════════════════════════════════════════════════════════════════════╝");
+                }
+                Console.WriteLine("Appuyer sur une touche pour revenir au menu.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else if (Gest == 2)
+            {
+                Console.WriteLine("Combien de travaux voulez-vous modifier ?");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1; i <= choice; i++)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Quel travail voulez-vous modifier ?");
+                    string modif = Console.ReadLine();
+                    string m = @"..\..\..\Config\Travaux_Sauvegarde\" + modif + ".json";
+                    try
+                    {
+                        File.Delete(m);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Travail introuvable");
+                        return;
+                    }
+                    Create_Travail_Sauvegarde(choice, nbFichiersSD);
+                }
+            }
+            else if (Gest == 3)
+            {
+                Console.Clear();
+                Console.WriteLine("Veuillez saisir le nom du travail à supprimer");
+                string supp = Console.ReadLine();
+                string f = @"..\..\..\Config\Travaux_Sauvegarde\" + supp + ".json";
+                try
+                {
+                    File.Delete(f);
+                    Console.Clear();
+                    Console.WriteLine("Suppression Terminée");
+
+                }
+                catch
+                {
+                    Console.WriteLine("Travail introuvable");
+                    return;
+                }
+
+            }
+            else if (Gest == 4)
+            {
+                Console.Clear();
+                return;
+            }
+            return;
+        }
+
     }
 }
