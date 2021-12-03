bf786f30294aeadc7ca69888b68df09795c8272d
Chiraz <Chirazmazouni16@gmail.com>
2021-12-03 13:03:59 +0100

    Chiraz

---
+++ Livrable 2/Easy_Save_GRP04/ModelView/CL_Create_Travail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using test;
+
+
+namespace EasySave_Grp4.ModelView
+{
+    class CL_Create_Travail
+    {
+        private Travail travail; // Déclarer vriable/objet de type travail
+        public CL_Create_Travail() //constructeur
+
+        {
+            travail = new Travail() ; 
+        }
+    }
+}
