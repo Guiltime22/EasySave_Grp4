1c2140a6fc5686032f12822324a3d40c2f6eb2b1
Guiltime22 <ghiles.ouchaoua.dz@viacesi.fr>
2021-12-04 01:27:20 +0100

    2.0

---
+++ Livrable 2/CryptoSoft/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace CryptoSoft
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int Taille_Argument = args.Length;
+            string source = args[0];
+            string destination = args[1];
+            DateTime Debut_Temps_Fichier = DateTime.Now;
+
+
+            for (int i = 0; i < Taille_Argument; i++)
+            {
+                if (args[i] == "source" && i + 1 < Taille_Argument)
+                {
+                    source = args[i + 1];
+                    i++;
+                }
+                else if (args[i] == "destination" && i + 1 < Taille_Argument)
+                {
+                    destination = args[i + 1];
+                    i++;
+                }
+            }
+
+            try
+            {
+                //Déclaration du tableau d'octets à crypter
+                byte[] Octet_A_Crypter = File.ReadAllBytes(source);
+                BitArray Bit_A_Crypter = new BitArray(Octet_A_Crypter);
+
+                //Déclaration du tableau de clé cryptage
+                byte[] Cle_En_Octet = new byte[8] { 7, 26, 106, 14, 143, 254, 112, 32 };
+                BitArray Cle_Par_Bit = new BitArray(Cle_En_Octet);
+
+                //Déclaration du tableau d'octets cryptés
+                byte[] Octet_Crypte = new byte[Octet_A_Crypter.Length];
+                BitArray Bit_Crypte = new BitArray(Bit_A_Crypter.Length);
+                int j = 0;
+
+                for (int i = 0; i < Bit_A_Crypter.Length; i++)
+                {
+                    j = i % Cle_En_Octet.Length;
+                    Bit_Crypte[i] = Bit_A_Crypter[i] ^ Cle_Par_Bit[j];
+                }
+
+                Bit_Crypte.CopyTo(Octet_Crypte, 0);
+
+                File.WriteAllBytes(destination, Octet_Crypte);
+                TimeSpan Temps_De_Cryptage = DateTime.Now - Debut_Temps_Fichier;
+                return (int)Temps_De_Cryptage.TotalMilliseconds;
+            }
+            catch
+            {
+                Console.WriteLine("Le fichier ne peut etre crypte");
+                return -1;
+            }
+        }
+    }
+}
---
+++ Livrable 2/EasySave_GRP4/Model/Parametres.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace EasySave_GRP4.Model
+{
+    public class JFile_parametres
+    {
+        public string Langue { get; set; }
+        public string Cryptage { get; set; }
+       
+    }
+    class Parametres
+    {
+        public void Parametres_Generaux(string Langue, string Cryptage)
+        {
+
+            var jFile_parametres = new JFile_parametres //Objects to insert into our JSON file
+            {
+                Langue = Langue,
+                Cryptage = Cryptage
+            };
+
+            string fileName = @"..\..\..\Config\Parametres.json"; // Name of the work 
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(jFile_parametres); // Convert the objects into string for JSON
+            File.WriteAllText(fileName, jsonString); // Create the work with the informations
+            MessageBox.Show("Parametres enrengistre");
+
+        }
+    }
+}
